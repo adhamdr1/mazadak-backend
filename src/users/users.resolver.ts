d87ae51a1d4227a145289e28e592b08769c3a755
Adham Mohamed Abdelkareem <adhamdr10@gmail.com>
2026-07-02 18:37:17 +0300

    feat: implement findUser query with admin-only access

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,10 +1,13 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from './entities/user.entity';
+import { User, UserRole } from './entities/user.entity';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import type { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { FindUserInput } from './dto/find-user.input';
 
 @Resolver(() => User)
 @UseGuards(JwtAuthGuard)
@@ -14,5 +17,12 @@ export class UsersResolver {
   @Query(() => User, { name: 'me' })
   async me(@CurrentUser() currentUser: JwtPayload): Promise<User> {
     return this.usersService.findById(currentUser.sub);
+  }
+
+  @Query(() => User, { name: 'findUser' })
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
+  async findUser(@Args('input') input: FindUserInput): Promise<User> {
+    return this.usersService.findById(input.id);
   }
 }
