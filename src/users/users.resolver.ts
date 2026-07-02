4bea0f55d78e4c90505f7a44fe433361077cf35d
Adham Mohamed Abdelkareem <adhamdr10@gmail.com>
2026-07-02 18:13:27 +0300

    feat: implement findMe UsersResolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
+import type { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
 
-@Resolver()
-export class UsersResolver {}
+@Resolver(() => User)
+@UseGuards(JwtAuthGuard)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => User, { name: 'me' })
+  async me(@CurrentUser() currentUser: JwtPayload): Promise<User> {
+    return this.usersService.findById(currentUser.sub);
+  }
+}
