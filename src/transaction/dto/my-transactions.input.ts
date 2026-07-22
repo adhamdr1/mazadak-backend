6d485e5a450c926ec0e649979cdc9f0abf43d5c3
Adham Mohamed Abdelkareem <adhamdr10@gmail.com>
2026-07-22 22:55:08 +0300

    refactor: unify pagination pattern, add @Max(100), remove health query

--- src/transaction/dto/my-transactions.input.ts
+++ src/transaction/dto/my-transactions.input.ts
@@ -1,20 +1,19 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
-import { IsInt, IsOptional, IsPositive, IsEnum } from 'class-validator';
+import { IsInt, IsOptional, IsPositive, IsEnum, Max } from 'class-validator';
 import { TransactionType } from '../enums/transaction-type.enum';
 
 @InputType()
 export class MyTransactionsInput {
-  @Field(() => Int, { nullable: true, defaultValue: 1 })
-  @IsOptional()
+  @Field(() => Int, { defaultValue: 1 })
   @IsInt()
   @IsPositive()
-  page?: number;
+  page: number = 1;
 
-  @Field(() => Int, { nullable: true, defaultValue: 10 })
-  @IsOptional()
+  @Field(() => Int, { defaultValue: 10 })
   @IsInt()
   @IsPositive()
-  limit?: number;
+  @Max(100, { message: 'Limit must not exceed 100' })
+  limit: number = 10;
 
   @Field(() => TransactionType, { nullable: true })
   @IsOptional()
